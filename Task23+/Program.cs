cd8fc00ffa0b7238c425d2c507f6dea35b3d6278
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-26 12:39:47 +0300

    Task23+ done

---
+++ Task23+/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number:");
+int N = int.Parse(Console.ReadLine());
+
+int Cube(int i)
+{
+    int x = i*i*i;
+    return x;
+}
+int i = 1;
+while (Cube(i) <= N)
+{
+    Console.Write($"{Cube(i)}, ");
+    i++;
+}
