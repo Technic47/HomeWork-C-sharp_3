52bb0217d8c5b3d4404ad8f4a8d3002053e0919c
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-26 12:27:59 +0300

    Task23 done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел
+
+Console.WriteLine("Enter number:");
+int N = int.Parse(Console.ReadLine());
+
+int[] array = new int[N];
+
+int Cube(int i)
+{
+    int x = i*i*i;
+    return x;
+}
+
+for (int i = 0; i < N; i++)
+{
+    array[i] = Cube((i+1));
+    Console.Write($"{array[i]}, ");
+}
+
